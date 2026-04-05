1be1b13daeb6c2d50c1f08ff395b15c3e83eae01
unboxedhypercharge-ctrl <unboxedhypercharge@gmail.com>
2026-04-06 01:59:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http, {
-  cors: { origin: "*" } 
-});
+const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-app.get('/', (req, res) => {
-  res.send('Watch Party Server is Awake!');
-});
+let roomState = {
+  time: 0,
+  isPlaying: false
+};
+
+app.get('/', (req, res) => { res.send('Watch Party Server is Awake!'); });
 
 io.on('connection', (socket) => {
-  console.log('A friend connected to the server!');
+  console.log('A friend connected!');
 
-  socket.on('ping_event', () => {
-    console.log('Someone clicked Ping!');
-    socket.broadcast.emit('pong_event'); 
-  });
- socket.on('check_ping', () => {
-    socket.emit('check_pong'); 
-  });
-   socket.on('play_video', (currentTime) => {
+  socket.emit('catch_up', roomState);
+
+  socket.on('play_video', (currentTime) => {
+    roomState.time = currentTime;
+    roomState.isPlaying = true;
     socket.broadcast.emit('sync_play', currentTime);
   });
 
-  // When someone pauses, tell everyone else to pause at that exact time
   socket.on('pause_video', (currentTime) => {
+    roomState.time = currentTime;
+    roomState.isPlaying = false;
     socket.broadcast.emit('sync_pause', currentTime);
   });
 
-  // When someone skips forward/backward, tell everyone else to skip
   socket.on('seek_video', (currentTime) => {
+    roomState.time = currentTime;
     socket.broadcast.emit('sync_seek', currentTime);
   });
-  socket.on('disconnect', () => {
-    console.log('A friend disconnected.');
+
+  socket.on('i_am_buffering', () => {
+    socket.broadcast.emit('force_wait');
   });
+
+  socket.on('disconnect', () => { console.log('A friend disconnected.'); });
 });
 
 const listener = http.listen(process.env.PORT || 3000, () => {
-  console.log('Server is listening on port ' + listener.address().port);
+  console.log('Server listening on port ' + listener.address().port);
 });
