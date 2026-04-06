aa9ac43be0112505245a4c761678c6fb93c04c21
unboxedhypercharge-ctrl <unboxedhypercharge@gmail.com>
2026-04-06 16:03:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,60 +3,88 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http, { cors: { origin: "*" } });
 
-let roomState = { time: 0, isPlaying: false, speed: 1, subtitle: null };
+const rooms = {}; 
 
 app.get('/', (req, res) => { res.send('Watch Party Server is Awake!'); });
 
 io.on('connection', (socket) => {
-  console.log('A friend connected!');
+  console.log('A user connected!');
+
+  socket.on('join_room', (roomId) => {
+    socket.join(roomId);
+    socket.roomId = roomId; 
+    if (!rooms[roomId]) {
+      rooms[roomId] = { time: 0, isPlaying: false, speed: 1, subtitle: null, users: 0 };
+    }
+    rooms[roomId].users++;
+    console.log(`User joined room: ${roomId}. Total users: ${rooms[roomId].users}`);
+  });
 
   socket.on('request_catch_up', () => {
-    socket.emit('catch_up', roomState);
+    if (socket.roomId && rooms[socket.roomId]) {
+      socket.emit('catch_up', rooms[socket.roomId]);
+    }
   });
 
   socket.on('play_video', (currentTime) => {
-    roomState.time = currentTime;
-    roomState.isPlaying = true;
-    io.emit('sync_play', currentTime);
+    if (!socket.roomId) return;
+    rooms[socket.roomId].time = currentTime;
+    rooms[socket.roomId].isPlaying = true;
+    io.to(socket.roomId).emit('sync_play', currentTime);
   });
 
   socket.on('pause_video', (currentTime) => {
-    roomState.time = currentTime;
-    roomState.isPlaying = false;
-    io.emit('sync_pause', currentTime);
+    if (!socket.roomId) return;
+    rooms[socket.roomId].time = currentTime;
+    rooms[socket.roomId].isPlaying = false;
+    io.to(socket.roomId).emit('sync_pause', currentTime);
   });
 
   socket.on('seek_video', (currentTime) => {
-    roomState.time = currentTime;
-    io.emit('sync_seek', currentTime);
+    if (!socket.roomId) return;
+    rooms[socket.roomId].time = currentTime;
+    io.to(socket.roomId).emit('sync_seek', currentTime);
   });
 
   socket.on('change_speed', (newSpeed) => {
-    roomState.speed = newSpeed;
-    io.emit('sync_speed', newSpeed);
+    if (!socket.roomId) return;
+    rooms[socket.roomId].speed = newSpeed;
+    io.to(socket.roomId).emit('sync_speed', newSpeed);
   });
 
   socket.on('heartbeat_time', (currentTime) => {
-    if (currentTime > roomState.time) {
-      roomState.time = currentTime;
+    if (!socket.roomId) return;
+    if (currentTime > rooms[socket.roomId].time) {
+      rooms[socket.roomId].time = currentTime;
     }
   });
 
   socket.on('upload_subtitle', (vttText) => {
-    roomState.subtitle = vttText; 
-    io.emit('sync_subtitle', vttText); 
+    if (!socket.roomId) return;
+    rooms[socket.roomId].subtitle = vttText; 
+    io.to(socket.roomId).emit('sync_subtitle', vttText); 
   });
 
-  socket.on('i_am_buffering', () => { io.emit('force_wait'); });
-  socket.on('buffer_resolved', () => { io.emit('clear_wait'); });
-  socket.on('disconnect', () => { console.log('A friend disconnected.'); });
-});
+  socket.on('i_am_buffering', () => { 
+    if (socket.roomId) io.to(socket.roomId).emit('force_wait'); 
+  });
+  
+  socket.on('buffer_resolved', () => { 
+    if (socket.roomId) io.to(socket.roomId).emit('clear_wait'); 
+  });
 
-setInterval(() => {
-  if (roomState.isPlaying) {
-    io.emit('auto_correction', roomState.time);
-  }
-}, 2000);
+  socket.on('disconnect', () => {
+    if (socket.roomId && rooms[socket.roomId]) {
+      rooms[socket.roomId].users--;
+      console.log(`User left room: ${socket.roomId}. Users left: ${rooms[socket.roomId].users}`);
+      
+      if (rooms[socket.roomId].users <= 0) {
+        delete rooms[socket.roomId];
+        console.log(`Room ${socket.roomId} deleted.`);
+      }
+    }
+  });
+});
 
 const listener = http.listen(process.env.PORT || 3000, () => {
   console.log('Server listening on port ' + listener.address().port);
